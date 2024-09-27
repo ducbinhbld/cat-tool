2f61024986ed0b8e8fdaaff2797efb7b9c855627
ducbinhbld <ducbinhbld@gmail.com>
2024-09-27 12:52:25 +0700

    Create tool for cat

---
+++ main.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const axios = require("axios");
+
+const users = JSON.parse(fs.readFileSync("user.json", "utf8"));
+const iframes = JSON.parse(fs.readFileSync("iframe.json", "utf8"));
+
+const getHeader = async (user) => {
+  return {
+    accept: "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    authorization: `tma ${user.auth}`,
+    "content-type": "application/json",
+    origin: "https://cats-frontend.tgapps.store",
+    priority: "u=1, i",
+    referer: "https://cats-frontend.tgapps.store/",
+    "sec-ch-ua":
+      '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+  };
+};
+
+const getTasks = async (user) => {
+  const header = await getHeader(user);
+  try {
+    const response = await axios.get(
+      "https://api.catshouse.club/tasks/user?group=cats",
+      { headers: header }
+    );
+    console.log("tasks", response.data.tasks);
+    const not_completed_tasks = response.data.tasks.filter(
+      (task) => !task.completed
+    );
+    return not_completed_tasks;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getTasksBitget = async (user) => {
+  const header = await getHeader(user);
+  try {
+    const response = await axios.get(
+      `https://api.catshouse.club/tasks/user?group=bitget`,
+      { headers: header }
+    );
+    console.log("tasksBitget", response.data.tasks);
+    return response.data.tasks;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const completeTask = async (user, task) => {
+  const header = await getHeader(user);
+  try {
+    const response = await axios.post(
+      `https://api.catshouse.club/tasks/${task?.id}/complete`,
+      {},
+      { headers: header }
+    );
+    if (response.data.success) {
+      console.log(`Đã hoàn thành task ${task?.id} là ${task?.title}`);
+    } else {
+      console.log(`"Không thể hoàn thành task ${task?.id} là ${task?.title} `);
+    }
+  } catch (e) {
+    console.log(`"Không thể hoàn thành task ${task.id} là ${task.title} `);
+  }
+};
+async function main() {
+  console.log("Start");
+  try {
+    for (const iframe of iframes) {
+      const web_app_data = Object.fromEntries(
+        new URLSearchParams(iframe.replace(/.*tgWebAppData/, "tgWebAppData"))
+      );
+      const user = web_app_data.tgWebAppData;
+      let tasks = [];
+      const normalTasks = await getTasks(user);
+      const tasksBitget = await getTasksBitget(user);
+      console.log("normalTasks", normalTasks);
+      console.log("tasksBitget", tasksBitget);
+      tasks = tasks.concat(normalTasks, tasksBitget);
+      for (let j = 0; j < tasks.length; j++) {
+        const task = tasks[j];
+        await completeTask(user, task);
+      }
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+main();
